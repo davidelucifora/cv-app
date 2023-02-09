cfaee06b41478b15b565c8f4736c2df62627223e
davidelucifora <davidelucifora1@gmail.com>
2023-02-09 23:11:29 +0000

    Added basic firestore user and CV saving logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import InputArea from "./components/input-area/input-area";
 import Preview from "./components/preview/preview";
-import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import { db } from "./utils/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 function App() {
+  const [user, setUser] = useState({});
+
   const [data, setData] = useState({
     contactInfo: "",
     shortBio: "",
@@ -13,11 +17,17 @@ function App() {
     education: [],
   });
 
+  function handleSaveData() {
+    setDoc(doc(db, "cvs", user.uid), data);
+  }
+
   return (
     <div className="App">
-      <Login />
-      <InputArea updateState={setData} />
-      <Preview data={data} />
+      <Navbar setUser={setUser} user={user} saveData={handleSaveData} />
+      <div className="flex">
+        <InputArea updateState={setData} />
+        <Preview data={data} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import { auth, db } from "./../utils/firebase";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+export default function Navbar(props) {
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+
+  // Watch if user is signed in or out
+  onAuthStateChanged(auth, (user) => {
+    if (user) props.setUser(user);
+    else props.setUser({});
+  });
+
+  function handleLogin() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+      });
+  }
+
+  function handleLogout() {
+    signOut(auth, provider)
+      .then((result) => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+      });
+  }
+
+  return (
+    <div className="navbar">
+      <button onClick={handleLogin}>
+        {props.user.displayName ? props.user.displayName : "Login"}
+      </button>
+      {props.user.displayName && (
+        <div className="logged-in-btns">
+          <button onClick={props.saveData}>Save Progress</button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </div>
+  );
+}
